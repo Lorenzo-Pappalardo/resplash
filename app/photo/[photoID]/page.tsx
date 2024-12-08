c41a772f82a095daf96e697b4084214fbfc2c3b4
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2024-12-08 17:17:43 +0100

    Improved the structure a bit

--- app/photo/[photoID]/page.tsx
+++ app/photo/[photoID]/page.tsx
@@ -7,29 +7,30 @@ import LoadingPhotoSkeleton from './loading';
 
 export const dynamicParams = true;
 
+const padding = 2;
+
 const PhotoDetailPage = async ({ params }: PhotoDetailPageProps) => {
   const photoID = (await params).photoID;
 
   const photo = await getSpecifiedPhoto(photoID);
 
   return (
-    <Box>
-      <Box position="absolute" top={16} left={16}>
+    <Box display="flex" flexDirection="column" overflow="hidden">
+      <Box padding={padding} alignSelf="start">
         <GoBackButton />
       </Box>
-      <Box padding={4} display="flex" justifyContent="center">
+      <Box display="flex" justifyContent="center">
         <Suspense fallback={<LoadingPhotoSkeleton />}>
           <img
             src={photo?.urls.full}
             alt={photo?.description ?? photo?.alt_description ?? ''}
             style={{
-              maxWidth: '90vw',
-              maxHeight: '90vh'
+              height: `calc(85vh - (${padding * 4} * var(--mui-spacing)))`
             }}
           />
         </Suspense>
       </Box>
-      <Box position="absolute" bottom={16} right={16}>
+      <Box padding={padding} alignSelf="end">
         {photo !== undefined && <LikeButton id={photo.id} />}
       </Box>
     </Box>
