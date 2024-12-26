9423d24d407f9d10d0fde15cac7e78d9a6795b89
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2024-12-26 10:34:56 +0100

    Set behaviour depending on authentication state

--- app/photo/[photoID]/likeButton.tsx
+++ app/photo/[photoID]/likeButton.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { likePhoto, unlikePhoto } from '@/app/actions/unsplash';
+import useGlobalStore from '@/app/state';
 import { Favorite } from '@mui/icons-material';
-import { Fab } from '@mui/material';
+import { Box, Fab, Tooltip } from '@mui/material';
 import { useState } from 'react';
 
 const LikeButton = ({ id }: { id?: string }) => {
+  const { authenticated } = useGlobalStore();
   const [liked, setLiked] = useState(false);
 
   const handleClick = async () => {
@@ -19,9 +21,25 @@ const LikeButton = ({ id }: { id?: string }) => {
   };
 
   return (
-    <Fab aria-label="like" color="primary" size="medium" onClick={handleClick}>
-      <Favorite htmlColor={liked ? 'red' : undefined} />
-    </Fab>
+    <Tooltip
+      title="Login to like"
+      placement="left"
+      disableFocusListener={authenticated}
+      disableHoverListener={authenticated}
+      disableInteractive={authenticated}
+      disableTouchListener={authenticated}>
+      {/* The Box below is necessary for the Tooltip to be able to listen to events and open accordingly */}
+      <Box>
+        <Fab
+          aria-label="like"
+          color="primary"
+          size="medium"
+          onClick={handleClick}
+          disabled={!authenticated}>
+          <Favorite htmlColor={liked ? 'red' : undefined} />
+        </Fab>
+      </Box>
+    </Tooltip>
   );
 };
 
--- app/state.ts
+++ app/state.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 
 const useGlobalStore = create<GlobalState>()(set => ({
+  authenticated: false,
   searchKeyword: '',
   page: 0,
   pageSize: 10,
+  setAuthenticated: authenticated => set({ authenticated }),
   setSearchKeyword: newSearchKeyword => set({ searchKeyword: newSearchKeyword }),
   setPage: newPage => set({ page: newPage }),
   setPageSize: newPageSize => set({ pageSize: newPageSize }),
@@ -14,9 +16,11 @@ const useGlobalStore = create<GlobalState>()(set => ({
 export default useGlobalStore;
 
 interface GlobalState {
+  authenticated: boolean;
   searchKeyword: string;
   page: number;
   pageSize: number;
+  setAuthenticated: (authenticated: boolean) => void;
   setSearchKeyword: (newSearchKeyword: string) => void;
   setPage: (newPage: number) => void;
   setPageSize: (newPageSize: number) => void;
