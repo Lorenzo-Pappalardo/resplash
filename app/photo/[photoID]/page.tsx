02eb4155208e0fc90c510fbf19a2bd9fd61a0453
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2024-12-29 12:36:36 +0100

    Set the photo to fit the available space

--- app/photo/[photoID]/page.tsx
+++ app/photo/[photoID]/page.tsx
@@ -1,9 +1,7 @@
 import { Box } from '@mui/material';
-import { Suspense } from 'react';
 import { getSpecifiedPhoto } from '../../actions/unsplash';
 import GoBackButton from './goBackButton';
 import LikeButton from './likeButton';
-import LoadingPhotoSkeleton from './loading';
 
 export const dynamicParams = true;
 
@@ -15,21 +13,18 @@ const PhotoDetailPage = async ({ params }: PhotoDetailPageProps) => {
   const photo = await getSpecifiedPhoto(photoID);
 
   return (
-    <Box display="flex" flexDirection="column" overflow="hidden">
+    <Box display="flex" flexDirection="column" height="100vh">
       <Box padding={padding} alignSelf="start">
         <GoBackButton />
       </Box>
-      <Box display="flex" justifyContent="center">
-        <Suspense fallback={<LoadingPhotoSkeleton />}>
-          <img
-            src={photo?.urls.full}
-            alt={photo?.description ?? photo?.alt_description ?? ''}
-            style={{
-              height: `calc(85vh - (${padding * 4} * var(--mui-spacing)))`
-            }}
-          />
-        </Suspense>
-      </Box>
+      <img
+        src={photo?.urls.full}
+        alt={photo?.description ?? photo?.alt_description ?? ''}
+        style={{
+          objectFit: 'contain',
+          height: '100%'
+        }}
+      />
       <Box padding={padding} alignSelf="end">
         {photo !== undefined && <LikeButton id={photo.id} />}
       </Box>
